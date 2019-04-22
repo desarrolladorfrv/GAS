1d0942b6fdb8d13027f8a9455b5329d7932b71fa
vazconcelos <desarrollador.frv@gmail.com>
2019-04-22 12:18:03 -0500

    vazconcelos

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,3 @@
 $("#divIframe").html("<iframe id='frameprueba' src='inicio.php'></iframe>");
+//$("#head").html("<iframe id='headFrame' src='headView.php'></iframe>");<-- es mi caso no debe de ser con iframe 
+//observacion esta bien con iframe pero aqui no deberia de ser asi deberias incrustarlo asi como incruste el encabezado
