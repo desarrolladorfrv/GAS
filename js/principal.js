4a41ba2f91ff8a0bfc7df0e885c93e446e9becae
vazconcelos <desarrollador.frv@gmail.com>
2019-04-22 12:28:57 -0500

    vazconcelos

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,1 @@
 $("#divIframe").html("<iframe id='frameprueba' src='inicio.php'></iframe>");
-//$("#head").html("<iframe id='headFrame' src='headView.php'></iframe>");<-- es mi caso no debe de ser con iframe 
